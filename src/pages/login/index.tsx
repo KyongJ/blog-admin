adf4b2aaf26e4a6d5a0325aee4e4144e53076b21
Kyong <kyongbest@163.com>
2024-03-19 22:26:11 +0800

    first

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Form, Input, Button, message } from 'antd';
+import { Form, Input, Button, message, Card, Select, Checkbox, Divider } from 'antd';
 import ReactCanvasNest from 'react-canvas-nest';
 import Logo from '@/assets/img/logo.png';
 import * as actions from '../../store/actions';
@@ -9,11 +9,30 @@ import { connect } from 'react-redux';
 import { login } from '../../api';
 import { setUserInfo } from '../../assets/js/publicFunc';
 import './index.less';
-
+const { Option } = Select;
 interface Props extends ReduxProps {}
 
 const Login: FC<Props> = ({ storeData: { userInfo = {} }, setStoreData }) => {
     const history = useHistory();
+    const [activeTabKey, setActiveTabKey] = useState<string>('phone');
+    const tabList = [
+        {
+            key: 'phone',
+            tab: '手机号',
+        },
+        {
+            key: 'email',
+            tab: '邮箱',
+        },
+    ];
+    const prefixSelector = (
+        <Form.Item name="prefix" noStyle>
+            <Select style={{ width: 70 }}>
+                <Option value="86">+86</Option>
+                <Option value="87">+87</Option>
+            </Select>
+        </Form.Item>
+    );
 
     // 触发登录方法
     const onFinish = async (values: any) => {
@@ -46,32 +65,51 @@ const Login: FC<Props> = ({ storeData: { userInfo = {} }, setStoreData }) => {
                 }}
                 style={{ zIndex: 1 }}
             />
-            <div className="logo-box">
+            {/* <div className="logo-box">
                 <img alt="加载失败" className="logo" src={Logo} />
-                <span className="logo-name">Blog-Admin</span>
-            </div>
-            <Form className="login-form" name="login-form" onFinish={onFinish}>
-                <Form.Item name="userName" rules={[{ required: true, message: '请输入用户名' }]}>
-                    <Input placeholder="用户名" prefix={<UserOutlined />} size="large" />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[{ required: true, message: '请输入密码' }]}
-                    extra="用户名:test 密码:123"
-                >
-                    <Input.Password placeholder="密码" prefix={<LockOutlined />} size="large" />
-                </Form.Item>
-                <Form.Item>
-                    <Button
-                        className="login-form-button"
-                        htmlType="submit"
-                        size="large"
-                        type="primary"
+                <span className="logo-name">欢迎使用</span>
+            </div> */}
+
+            <Card
+                className="login-form"
+                title={<span className="logo-name">欢迎使用</span>}
+                tabList={tabList}
+                activeTabKey={activeTabKey}
+                style={{ width: 350, fontSize: 14 }}
+            >
+                <Form name="login-form" onFinish={onFinish}>
+                    <Form.Item
+                        name="phone"
+                        rules={[{ required: true, message: 'Please input your phone number!' }]}
                     >
-                        登录
-                    </Button>
-                </Form.Item>
-            </Form>
+                        <Input
+                            addonBefore={prefixSelector}
+                            style={{ width: '100%' }}
+                            placeholder="请输入你的手机号"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button
+                            className="login-form-button"
+                            htmlType="submit"
+                            size="large"
+                            type="primary"
+                            style={{ borderRadius: 6 }}
+                        >
+                            下一步
+                        </Button>
+                    </Form.Item>
+                    <Form.Item name="agreement" valuePropName="checked">
+                        <Checkbox>
+                            <span style={{ fontSize: 12 }}>
+                                我已经阅读并同意 <a href="">服务协议</a> 和 <a href="">隐私策略</a>
+                            </span>
+                        </Checkbox>
+                    </Form.Item>
+                </Form>
+                <div style={{marginTop:100}}></div>
+                <Divider  plain>其他方式登录</Divider>
+            </Card>
         </div>
     );
 };
